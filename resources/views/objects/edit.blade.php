9bb0ca4a250078d27c8fb4b8c10a1340d9d16c82
Vlad Samsonov <Vlad Samsonov>
2021-11-29 12:55:12 +0300

    fix

--- resources/views/objects/edit.blade.php
+++ resources/views/objects/edit.blade.php
@@ -15,102 +15,100 @@
                 <div class="card-body py-3">
                     <form class="form" action="{{ route('objects.update', $object) }}" method="POST" enctype="multipart/form-data">
                         @csrf
-                        <div class="scroll-y me-n7 pe-7" id="kt_modal_new_address_scroll" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_modal_new_address_header" data-kt-scroll-wrappers="#kt_modal_new_address_scroll" data-kt-scroll-offset="300px">
-                            <div class="row mb-5">
-                                <div class="col-md-12 fv-row">
-                                    <div class="mb-10 fv-row" data-kt-password-meter="true">
-                                        <div class="mb-1">
-                                            <label class="form-label fw-bolder text-dark fs-6">Код</label>
-                                            <div class="position-relative mb-3">
-                                                <input
-                                                    class="form-control form-control-lg form-control-solid {{ $errors->has('code') ? 'is-invalid' : '' }}"
-                                                    type="text"
-                                                    name="code"
-                                                    value="{{ old('code', $object->code) }}"
-                                                    required
-                                                    autofocus
-                                                />
-                                            </div>
-                                            @if ($errors->has('code'))
-                                                <div class="fv-plugins-message-container invalid-feedback">
-                                                    <div>{{ implode(' ', $errors->get('code')) }}</div>
-                                                </div>
-                                            @endif
+                        <div class="row mb-5">
+                            <div class="col-md-12 fv-row">
+                                <div class="mb-10 fv-row" data-kt-password-meter="true">
+                                    <div class="mb-1">
+                                        <label class="form-label fw-bolder text-dark fs-6">Код</label>
+                                        <div class="position-relative mb-3">
+                                            <input
+                                                class="form-control form-control-lg form-control-solid {{ $errors->has('code') ? 'is-invalid' : '' }}"
+                                                type="text"
+                                                name="code"
+                                                value="{{ old('code', $object->code) }}"
+                                                required
+                                                autofocus
+                                            />
                                         </div>
-                                    </div>
-
-                                    <div class="mb-10 fv-row" data-kt-password-meter="true">
-                                        <div class="mb-1">
-                                            <label class="form-label fw-bolder text-dark fs-6">Название</label>
-                                            <div class="position-relative mb-3">
-                                                <input
-                                                    class="form-control form-control-lg form-control-solid {{ $errors->has('name') ? 'is-invalid' : '' }}"
-                                                    type="text"
-                                                    name="name"
-                                                    value="{{ old('name', $object->name) }}"
-                                                    required
-                                                />
-                                            </div>
-                                            @if ($errors->has('name'))
-                                                <div class="fv-plugins-message-container invalid-feedback">
-                                                    <div>{{ implode(' ', $errors->get('name')) }}</div>
-                                                </div>
-                                            @endif
-                                        </div>
-                                    </div>
-
-                                    <div class="fv-row mb-10">
-                                        <label class="form-label fw-bolder text-dark fs-6">Адрес</label>
-                                        <input
-                                            class="form-control form-control-lg form-control-solid {{ $errors->has('address') ? 'is-invalid' : '' }}"
-                                            type="text"
-                                            value="{{ old('address', $object->address) }}"
-                                            name="address"
-                                        />
-                                        @if ($errors->has('address'))
+                                        @if ($errors->has('code'))
                                             <div class="fv-plugins-message-container invalid-feedback">
-                                                <div>{{ implode(' ', $errors->get('address')) }}</div>
+                                                <div>{{ implode(' ', $errors->get('code')) }}</div>
                                             </div>
                                         @endif
                                     </div>
+                                </div>
 
-                                    <div class="fv-row mb-10">
-                                        <label class="col-lg-4 col-form-label fw-bold fs-6">Фотография</label>
-                                        <div class="col-lg-8">
-                                            <div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url({{ asset('images/blanks/object_photo_blank.jpg') }})">
-                                                <div class="image-input-wrapper w-125px h-125px" style="background-image: url({{ $object->getPhoto() }})"></div>
-                                                <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Изменить фото">
-                                                    <i class="bi bi-pencil-fill fs-7"></i>
-                                                    <input type="file" name="photo" accept=".png, .jpg, .jpeg" />
-                                                    <input type="hidden" name="avatar_remove" />
-                                                </label>
-                                                <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" title="Отменить фото">
+                                <div class="mb-10 fv-row" data-kt-password-meter="true">
+                                    <div class="mb-1">
+                                        <label class="form-label fw-bolder text-dark fs-6">Название</label>
+                                        <div class="position-relative mb-3">
+                                            <input
+                                                class="form-control form-control-lg form-control-solid {{ $errors->has('name') ? 'is-invalid' : '' }}"
+                                                type="text"
+                                                name="name"
+                                                value="{{ old('name', $object->name) }}"
+                                                required
+                                            />
+                                        </div>
+                                        @if ($errors->has('name'))
+                                            <div class="fv-plugins-message-container invalid-feedback">
+                                                <div>{{ implode(' ', $errors->get('name')) }}</div>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="fv-row mb-10">
+                                    <label class="form-label fw-bolder text-dark fs-6">Адрес</label>
+                                    <input
+                                        class="form-control form-control-lg form-control-solid {{ $errors->has('address') ? 'is-invalid' : '' }}"
+                                        type="text"
+                                        value="{{ old('address', $object->address) }}"
+                                        name="address"
+                                    />
+                                    @if ($errors->has('address'))
+                                        <div class="fv-plugins-message-container invalid-feedback">
+                                            <div>{{ implode(' ', $errors->get('address')) }}</div>
+                                        </div>
+                                    @endif
+                                </div>
+
+                                <div class="fv-row mb-10">
+                                    <label class="col-lg-4 col-form-label fw-bold fs-6">Фотография</label>
+                                    <div class="col-lg-8">
+                                        <div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url({{ asset('images/blanks/object_photo_blank.jpg') }})">
+                                            <div class="image-input-wrapper w-125px h-125px" style="background-image: url({{ $object->getPhoto() }})"></div>
+                                            <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Изменить фото">
+                                                <i class="bi bi-pencil-fill fs-7"></i>
+                                                <input type="file" name="photo" accept=".png, .jpg, .jpeg" />
+                                                <input type="hidden" name="avatar_remove" />
+                                            </label>
+                                            <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" title="Отменить фото">
+                            <i class="bi bi-x fs-2"></i>
+                        </span>
+                                            @if ($object->photo)
+                                                <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Удалить фото">
                                 <i class="bi bi-x fs-2"></i>
                             </span>
-                                                @if ($object->photo)
-                                                    <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Удалить фото">
-                                    <i class="bi bi-x fs-2"></i>
-                                </span>
-                                                @endif
-                                            </div>
-                                            <div class="form-text">Поддерживаются форматы: <code>png, jpg, jpeg</code></div>
-                                            @if ($errors->has('photo'))
-                                                <div class="fv-plugins-message-container invalid-feedback">
-                                                    <div>{{ implode(' ', $errors->get('photo')) }}</div>
-                                                </div>
                                             @endif
                                         </div>
+                                        <div class="form-text">Поддерживаются форматы: <code>png, jpg, jpeg</code></div>
+                                        @if ($errors->has('photo'))
+                                            <div class="fv-plugins-message-container invalid-feedback">
+                                                <div>{{ implode(' ', $errors->get('photo')) }}</div>
+                                            </div>
+                                        @endif
                                     </div>
+                                </div>
 
-                                    <div class="mb-10 fv-row">
-                                        <label class="form-label fw-bolder text-dark fs-6">Статус</label>
-                                        <div class="position-relative mb-3">
-                                            <select name="status_id" data-control="select2" class="form-select form-select-solid form-select-lg">
-                                                @foreach($statuses as $statusId => $status)
-                                                    <option value="{{ $statusId }}" {{ $statusId === $object->status_id ? 'selected' : '' }}>{{ $status }}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
+                                <div class="mb-10 fv-row">
+                                    <label class="form-label fw-bolder text-dark fs-6">Статус</label>
+                                    <div class="position-relative mb-3">
+                                        <select name="status_id" data-control="select2" class="form-select form-select-solid form-select-lg">
+                                            @foreach($statuses as $statusId => $status)
+                                                <option value="{{ $statusId }}" {{ $statusId === $object->status_id ? 'selected' : '' }}>{{ $status }}</option>
+                                            @endforeach
+                                        </select>
                                     </div>
                                 </div>
                             </div>
