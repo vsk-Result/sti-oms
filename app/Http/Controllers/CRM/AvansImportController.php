a98137f3aeb74c1c47d18452f7021ae529795741
Vlad Samsonov <Vlad Samsonov>
2021-12-06 11:13:11 +0300

    fix

--- app/Http/Controllers/CRM/AvansImportController.php
+++ app/Http/Controllers/CRM/AvansImportController.php
@@ -21,18 +21,7 @@ class AvansImportController extends Controller
                 $sum += $item->avans->value;
             }
 
-            \Log::info($import->id . ' ' . $sum . ' | ' . $paymentAmount);
-            \Log::info($sum === $paymentAmount ? 'true' : 'false');
-            \Log::info($sum == $paymentAmount ? 'true' : 'false');
-            \Log::info((float) $sum == $paymentAmount ? 'true' : 'false');
-            \Log::info(gettype($paymentAmount));
-            \Log::info(gettype($sum));
-            \Log::info($sum == 69670.43);
-            \Log::info($paymentAmount == 69670.43);
-            \Log::info((string) $sum == (string) $paymentAmount);
-            \Log::info((string) $sum === (string) $paymentAmount);
-
-            if ($sum === $paymentAmount) {
+            if ((string) $sum === (string) $paymentAmount) {
                 $imports[$import->id] = Carbon::parse($import->date)->format('d.m.Y') . ' | ' . number_format($sum, 2, '.', ' ') . ' | ' . $import->description;
             }
         }
