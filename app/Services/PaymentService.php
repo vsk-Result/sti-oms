d4febcf04526c7fd9a2c14734787902d6da1fe79
Vlad Samsonov <Vlad Samsonov>
2021-12-13 14:36:42 +0300

    fix

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -85,7 +85,7 @@ class PaymentService
 
         if (! empty($requestData['amount_expression'])) {
             $expression = str_replace(',', '.', $requestData['amount_expression']);
-            $expression = preg_replace("/[^<>=!.0-9]/", '', $expression);
+            $expression = preg_replace("/[^-<>=!.0-9]/", '', $expression);
 
             $operators = ['<=', '<', '>=', '>', '!=', '='];
             foreach ($operators as $operator) {
