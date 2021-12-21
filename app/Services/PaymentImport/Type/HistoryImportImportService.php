e74a720e604f5e70696636256d142845075e5894
Vlad Samsonov <Vlad Samsonov>
2021-12-21 15:54:02 +0300

    fix

--- app/Services/PaymentImport/Type/HistoryImportImportService.php
+++ app/Services/PaymentImport/Type/HistoryImportImportService.php
@@ -34,7 +34,7 @@ class HistoryImportImportService
     public function createImport(array $requestData): null|PaymentImport
     {
         $historyData = $this->getHistoryDataFromExcel($requestData['file']);
-
+        \Log::info('Data received!');
         if (empty($historyData)) {
             return null;
         }
@@ -48,7 +48,7 @@ class HistoryImportImportService
             'file' => $this->uploadService->uploadFile('payment-imports/history', $requestData['file']),
             'description' => 'Загружен файл ' . $requestData['file']->getClientOriginalName()
         ]);
-
+        \Log::info('Import created!');
         $companyOrganization = $this->organizationService->getOrCreateOrganization([
             'company_id' => $import->company->id,
             'name' => $import->company->name,
@@ -57,7 +57,9 @@ class HistoryImportImportService
         ]);
 
         $allPayments = Payment::all();
+        \Log::info('All payments received!');
         $this->paymentService->loadCategoriesList();
+        \Log::info('Categories list loaded!');
         foreach ($historyData[0] as $index => $paymentData) {
             if ($index === 0) {
                 continue;
