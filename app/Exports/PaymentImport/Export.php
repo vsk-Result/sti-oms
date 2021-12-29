69b13e184bc55e001c02612fb1b56ddfdeb58d51
Vlad Samsonov <Vlad Samsonov>
2021-12-29 11:22:39 +0300

    fix

--- app/Exports/PaymentImport/Export.php
+++ app/Exports/PaymentImport/Export.php
@@ -22,29 +22,29 @@ class Export implements WithMultipleSheets
     public function sheets(): array
     {
         $sheets = [];
-//        $groupedByTypePayments = $this->payments->sortBy('type_id')->groupBy('type_id');
-//
-//        foreach ($groupedByTypePayments as $type => $gPayments) {
-//            switch ($type) {
-//                case Payment::TYPE_NONE:
-//                    $sheets[] = new ObjectSheet('Без кода', $gPayments);
-//                    break;
-//                case Payment::TYPE_OBJECT:
-//                    $groupedPayments = $gPayments->sortBy('object_id')->groupBy('object_id');
-//                    foreach ($groupedPayments as $objectId => $payments) {
-//                        $sheets[] = new ObjectSheet(BObject::find($objectId)->code, $payments);
-//                    }
-//                    break;
-//                case Payment::TYPE_GENERAL:
-//                    $sheets[] = new GeneralSheet($gPayments);
-//                    break;
-//                case Payment::TYPE_TRANSFER:
-//                    $sheets[] = new TransferSheet($gPayments);
-//                    break;
-//                default:
-//                    break;
-//            }
-//        }
+        $groupedByTypePayments = $this->payments->sortBy('type_id')->groupBy('type_id');
+
+        foreach ($groupedByTypePayments as $type => $gPayments) {
+            switch ($type) {
+                case Payment::TYPE_NONE:
+                    $sheets[] = new ObjectSheet('Без кода', $gPayments);
+                    break;
+                case Payment::TYPE_OBJECT:
+                    $groupedPayments = $gPayments->sortBy('object_id')->groupBy('object_id');
+                    foreach ($groupedPayments as $objectId => $payments) {
+                        $sheets[] = new ObjectSheet(BObject::find($objectId)->code, $payments);
+                    }
+                    break;
+                case Payment::TYPE_GENERAL:
+                    $sheets[] = new GeneralSheet($gPayments);
+                    break;
+                case Payment::TYPE_TRANSFER:
+                    $sheets[] = new TransferSheet($gPayments);
+                    break;
+                default:
+                    break;
+            }
+        }
 
         $payments = $this->payments->sortBy('type_id')->sortBy('object_id');
         $sheets[] = new ObjectSheet('Общая таблица', $payments);
