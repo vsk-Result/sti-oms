60f155e9fa3901edd055a765da4e7a8ad6dcd873
Vlad Samsonov <Vlad Samsonov>
2021-12-30 12:26:19 +0300

    fix

--- app/Exports/Payment/Export.php
+++ app/Exports/Payment/Export.php
@@ -4,6 +4,7 @@ namespace App\Exports\Payment;
 
 use App\Exports\Payment\Sheets\PaymentSheet;
 use App\Exports\Payment\Sheets\PivotSheet;
+use App\Models\Object\BObject;
 use Illuminate\Database\Eloquent\Builder;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
@@ -18,8 +19,14 @@ class Export implements WithMultipleSheets
 
     public function sheets(): array
     {
+        $object = null;
+        $objectIds = (clone $this->payments)->pluck('object_id')->unique('object_id');
+        if (count($objectIds) === 1) {
+            $object = BObject::find($objectIds[0])->getName() ?? '';
+        }
+
         return [
-            new PivotSheet('Сводная', $this->payments),
+            new PivotSheet('Сводная', $this->payments, $object),
             new PaymentSheet('Таблица оплат', $this->payments, $this->payments->count())
         ];
     }
--- app/Exports/Payment/Sheets/PivotSheet.php
+++ app/Exports/Payment/Sheets/PivotSheet.php
@@ -2,6 +2,7 @@
 
 namespace App\Exports\Payment\Sheets;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\WithTitle;
@@ -18,10 +19,13 @@ class PivotSheet implements
 
     private Builder $payments;
 
-    public function __construct(string $sheetName, Builder $payments)
+    private null|string $object;
+
+    public function __construct(string $sheetName, Builder $payments, null|string $object)
     {
         $this->sheetName = $sheetName;
         $this->payments = $payments;
+        $this->object = $object;
     }
 
     public function title(): string
@@ -33,55 +37,60 @@ class PivotSheet implements
     {
         $sheet->getParent()->getDefaultStyle()->getFont()->setName('Calibri')->setSize(11);
 
-        $sheet->setCellValue('A2', 'Приход');
-        $sheet->setCellValue('A3', 'Расход');
-        $sheet->setCellValue('A4', 'Баланс');
-        $sheet->setCellValue('B1', 'Итого');
-        $sheet->setCellValue('C1', 'Итого, без НДС');
+        $sheet->setCellValue('A1', $this->object);
+        $sheet->setCellValue('B1', 'с ' . Carbon::parse((clone $this->payments)->min('date'))->format('d.m.Y'));
+        $sheet->setCellValue('C1', 'по ' . Carbon::parse((clone $this->payments)->max('date'))->format('d.m.Y'));
+
+        $sheet->setCellValue('A3', 'Приход');
+        $sheet->setCellValue('A4', 'Расход');
+        $sheet->setCellValue('A5', 'Баланс');
+        $sheet->setCellValue('B2', 'Итого');
+        $sheet->setCellValue('C2', 'Итого, без НДС');
 
         $sheet->getRowDimension(1)->setRowHeight(30);
         $sheet->getRowDimension(2)->setRowHeight(30);
         $sheet->getRowDimension(3)->setRowHeight(30);
         $sheet->getRowDimension(4)->setRowHeight(30);
+        $sheet->getRowDimension(5)->setRowHeight(30);
 
-        $sheet->getColumnDimension('A')->setWidth(22);
+        $sheet->getColumnDimension('A')->setWidth(30);
         $sheet->getColumnDimension('B')->setWidth(22);
         $sheet->getColumnDimension('C')->setWidth(22);
 
-        $sheet->getStyle('A4:C4')->getFont()->setBold(true);
+        $sheet->getStyle('A5:C5')->getFont()->setBold(true);
 
-        $sheet->getStyle('A1:C3')->applyFromArray([
+        $sheet->getStyle('A1:C4')->applyFromArray([
             'borders' => ['allBorders' => ['borderStyle' => Border::BORDER_THIN]]
         ]);
 
-        $sheet->getStyle('A4:C4')->applyFromArray([
+        $sheet->getStyle('A5:C5')->applyFromArray([
             'borders' => ['allBorders' => ['borderStyle' => Border::BORDER_MEDIUM]]
         ]);
 
-        $sheet->getStyle('B2:C2')->getFont()->setColor(new Color(Color::COLOR_DARKGREEN));
-        $sheet->getStyle('B3:C3')->getFont()->setColor(new Color(Color::COLOR_RED));
+        $sheet->getStyle('B3:C3')->getFont()->setColor(new Color(Color::COLOR_DARKGREEN));
+        $sheet->getStyle('B4:C4')->getFont()->setColor(new Color(Color::COLOR_RED));
 
-        $sheet->getStyle('A1:A4')->getAlignment()->setVertical('center')->setHorizontal('left')->setWrapText(false);
-        $sheet->getStyle('B1:C4')->getAlignment()->setVertical('center')->setHorizontal('center')->setWrapText(false);
+        $sheet->getStyle('A1:A5')->getAlignment()->setVertical('center')->setHorizontal('left')->setWrapText(true);
+        $sheet->getStyle('B1:C5')->getAlignment()->setVertical('center')->setHorizontal('center')->setWrapText(false);
 
-        $sheet->getStyle('B2:C4')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
+        $sheet->getStyle('B3:C5')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
 
-        $sheet->setCellValue('B2', (clone $this->payments)->where('amount', '>=', 0)->sum('amount'));
-        $sheet->setCellValue('C2', (clone $this->payments)->where('amount', '>=', 0)->sum('amount_without_nds'));
+        $sheet->setCellValue('B3', (clone $this->payments)->where('amount', '>=', 0)->sum('amount'));
+        $sheet->setCellValue('C3', (clone $this->payments)->where('amount', '>=', 0)->sum('amount_without_nds'));
 
-        $sheet->setCellValue('B3', (clone $this->payments)->where('amount', '<', 0)->sum('amount'));
-        $sheet->setCellValue('C3', (clone $this->payments)->where('amount', '<', 0)->sum('amount_without_nds'));
+        $sheet->setCellValue('B4', (clone $this->payments)->where('amount', '<', 0)->sum('amount'));
+        $sheet->setCellValue('C4', (clone $this->payments)->where('amount', '<', 0)->sum('amount_without_nds'));
 
         $totalAmount = (clone $this->payments)->sum('amount');
         $totalAmountWithoutNDS = (clone $this->payments)->sum('amount_without_nds');
 
         if ($totalAmount < 0) {
-            $sheet->getStyle('B4:C4')->getFont()->setColor(new Color(Color::COLOR_RED));
+            $sheet->getStyle('B5:C5')->getFont()->setColor(new Color(Color::COLOR_RED));
         } else {
-            $sheet->getStyle('B4:C4')->getFont()->setColor(new Color(Color::COLOR_DARKGREEN));
+            $sheet->getStyle('B5:C5')->getFont()->setColor(new Color(Color::COLOR_DARKGREEN));
         }
 
-        $sheet->setCellValue('B4', $totalAmount);
-        $sheet->setCellValue('C4', $totalAmountWithoutNDS);
+        $sheet->setCellValue('B5', $totalAmount);
+        $sheet->setCellValue('C5', $totalAmountWithoutNDS);
     }
 }
