726a9ecd0ee22fb93b6f73ae932110d926709af0
Vlad <result007@yandex.ru>
2022-04-20 17:56:33 +0300

    fix

--- app/Http/Controllers/Company/CompanyController.php
+++ app/Http/Controllers/Company/CompanyController.php
@@ -95,7 +95,6 @@ class CompanyController extends Controller
             ->where('description', '!=', 'PATRIOT')
             ->where(function($q) {
                 $q->where('bank_id', 1);
-                $q->orWhere('bank_id', 3);
                 $q->orWhere('bank_id', null);
             })
             ->where(function($q) use ($PTIOrganization) {
