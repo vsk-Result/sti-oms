8e2343c1538f1b1879661ed64791fe7adc42cccc
Vlad <result007@yandex.ru>
2022-06-02 17:52:50 +0300

    fix

--- app/Services/FinanceReport/LoanService.php
+++ app/Services/FinanceReport/LoanService.php
@@ -25,6 +25,7 @@ class LoanService
         $totalLoanDTAmount = -Payment::where('date', '>=', '2020-12-17')
             ->where('date', '<=', $date)
             ->where('type_id', Payment::TYPE_TRANSFER)
+            ->whereNotIn('bank_id', [6])
             ->where('payment_type_id', Payment::PAYMENT_TYPE_NON_CASH)
             ->where(function ($q) use ($DTOrganization) {
                 $q->where('organization_receiver_id', $DTOrganization->id);
