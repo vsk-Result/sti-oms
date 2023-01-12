0e312818700eb974c039b842e38b79f9601ceb7e
result <result007@yandex.ru>
2023-01-12 19:59:36 +0300

    add visibility in bank; change bank name;

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -8,54 +8,80 @@ class Bank
         [
             'id' => 1,
             'name' => 'ПАО "ВТБ"',
+            'logo' => '/images/banks/vtb.png',
+            'visible' => true
         ],
         [
             'id' => 2,
             'name' => 'ПАО "Промсвязьбанк"',
+            'logo' => '/images/banks/promsvyazbank.png',
+            'visible' => true
         ],
         [
             'id' => 3,
             'name' => 'ПАО "Сбербанк"',
+            'logo' => '/images/banks/sber.png',
+            'visible' => true
         ],
         [
             'id' => 4,
             'name' => 'ПАО "Совкомбанк"',
+            'logo' => '/images/banks/sovkombank.jpg',
+            'visible' => true
         ],
         [
             'id' => 5,
             'name' => 'ПАО "Росбанк"',
+            'logo' => '/images/banks/rosbank.png',
+            'visible' => true
         ],
         [
             'id' => 6,
             'name' => 'АО "КУБ"',
+            'logo' => '/images/banks/kub.png',
+            'visible' => true
         ],
         [
             'id' => 7,
             'name' => 'ПАО "МКБ"',
+            'logo' => '/images/banks/mkb.png',
+            'visible' => true
         ],
         [
             'id' => 8,
             'name' => 'АО "ОТП"',
+            'logo' => '/images/banks/otp.jpg',
+            'visible' => true
         ],
         [
             'id' => 9,
             'name' => 'АО "ЮниКредит Банк"',
+            'logo' => '/images/banks/unicredit.jpg',
+            'visible' => true
         ],
         [
             'id' => 10,
-            'name' => 'АКБ "АБСОЛЮТ БАНК" (ПАО)',
+            'name' => 'АКБ "АБСОЛЮТ БАНК"',
+            'logo' => '/images/banks/ab.png',
+            'visible' => true
         ],
         [
             'id' => 11,
             'name' => 'АО "АЛЬФА БАНК"',
+            'logo' => '/images/banks/alfa.jpg',
+            'visible' => true
         ],
         [
             'id' => 12,
             'name' => 'ВТБ Тинькофф',
+            'logo' => '/images/banks/vtb.png',
+            'visible' => false
         ],
         [
             'id' => 13,
-            'name' => 'ВТБ Мульти',
+            'name' => 'ВТБ Тинькофф',
+            'logo' => '/images/banks/vtb.png',
+            'visible' => true
         ],
     ];
 
@@ -63,7 +89,9 @@ class Bank
     {
         $banks = [];
         foreach (static::$banks as $bank) {
-            $banks[$bank['id']] = $bank['name'];
+            if ($bank['visible']) {
+                $banks[$bank['id']] = $bank['name'];
+            }
         }
         return $banks;
     }
@@ -71,5 +99,10 @@ class Bank
     public static function getBankName(int $bankId): string
     {
         return static::$banks[$bankId - 1]['name'];
+    }
+
+    public static function getBankLogo(int $bankId): string
+    {
+        return static::$banks[$bankId - 1]['logo'];
     }
 }
