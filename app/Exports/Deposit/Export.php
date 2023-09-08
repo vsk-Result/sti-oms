e9b0983ecc3f6a177234337d17aa83e494960bf7
result <result007@yandex.ru>
2023-09-08 13:35:47 +0300

    refactor deposit

--- app/Exports/Deposit/Export.php
+++ app/Exports/Deposit/Export.php
@@ -36,45 +36,42 @@ class Export implements
         $sheet->getParent()->getDefaultStyle()->getFont()->setName('Calibri')->setSize(11);
 
         $sheet->setCellValue('A1', 'Объект');
-        $sheet->setCellValue('B1', 'Договор');
-        $sheet->setCellValue('C1', 'Валюта');
-        $sheet->setCellValue('D1', 'Контрагент');
-        $sheet->setCellValue('E1', 'Дата начала');
-        $sheet->setCellValue('F1', 'Дата окончания');
-        $sheet->setCellValue('G1', 'Сумма');
+        $sheet->setCellValue('B1', 'Валюта');
+        $sheet->setCellValue('C1', 'Дата начала');
+        $sheet->setCellValue('D1', 'Дата окончания');
+        $sheet->setCellValue('E1', 'Сумма');
+        $sheet->setCellValue('F1', 'Описание');
 
         $sheet->getRowDimension(1)->setRowHeight(35);
 
         $sheet->getColumnDimension('A')->setWidth(12);
-        $sheet->getColumnDimension('B')->setWidth(35);
-        $sheet->getColumnDimension('C')->setWidth(12);
+        $sheet->getColumnDimension('B')->setWidth(12);
+        $sheet->getColumnDimension('C')->setWidth(35);
         $sheet->getColumnDimension('D')->setWidth(35);
-        $sheet->getColumnDimension('E')->setWidth(22);
-        $sheet->getColumnDimension('F')->setWidth(22);
-        $sheet->getColumnDimension('G')->setWidth(17);
+        $sheet->getColumnDimension('E')->setWidth(35);
+        $sheet->getColumnDimension('F')->setWidth(50);
 
-        $sheet->getStyle('A1:G1')->getFont()->setBold(true);
+        $sheet->getStyle('A1:F1')->getFont()->setBold(true);
 
         $row = 2;
         foreach ($this->deposits as $deposit) {
             $sheet->setCellValue('A' . $row, $deposit->object->code);
-            $sheet->setCellValue('B' . $row, $deposit->contract?->getName());
-            $sheet->setCellValue('C' . $row, $deposit->currency);
-            $sheet->setCellValue('D' . $row, $deposit->organization?->name);
-            $sheet->setCellValue('E' . $row, Date::dateTimeToExcel(Carbon::parse($deposit->start_date)));
-            $sheet->setCellValue('F' . $row, Date::dateTimeToExcel(Carbon::parse($deposit->end_date)));
-            $sheet->setCellValue('G' . $row, $deposit->amount);
+            $sheet->setCellValue('B' . $row, $deposit->currency);
+            $sheet->setCellValue('C' . $row, Date::dateTimeToExcel(Carbon::parse($deposit->start_date)));
+            $sheet->setCellValue('D' . $row, Date::dateTimeToExcel(Carbon::parse($deposit->end_date)));
+            $sheet->setCellValue('E' . $row, $deposit->amount);
+            $sheet->setCellValue('F' . $row, $deposit->description);
 
             $sheet->getRowDimension($row)->setRowHeight(25);
             $row++;
         }
         $row--;
 
-        $sheet->getStyle('A1:G' . $row)->applyFromArray($THINStyleArray);
-        $sheet->getStyle('A1:G' . $row)->getAlignment()->setVertical('center')->setHorizontal('center')->setWrapText(false);
-        $sheet->getStyle('G2:G' . $row)->getNumberFormat()->setFormatCode('_-* #,##0_-;-* #,##0_-;_-* "-"_-;_-@_-');
-        $sheet->getStyle('E2:F' . $row)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_DDMMYYYY);
+        $sheet->getStyle('A1:F' . $row)->applyFromArray($THINStyleArray);
+        $sheet->getStyle('A1:F' . $row)->getAlignment()->setVertical('center')->setHorizontal('center')->setWrapText(false);
+        $sheet->getStyle('E2:E' . $row)->getNumberFormat()->setFormatCode('_-* #,##0_-;-* #,##0_-;_-* "-"_-;_-@_-');
+        $sheet->getStyle('C2:D' . $row)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_DATE_DDMMYYYY);
 
-        $sheet->setAutoFilter('A1:G' . $row);
+        $sheet->setAutoFilter('A1:F' . $row);
     }
 }
