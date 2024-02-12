d11ced14df6b6e65937c5e3c597297fa4c92bffb
result <result007@yandex.ru>
2024-02-13 00:37:22 +0300

    fix

--- resources/views/objects/parts/debts-details/workers_salary.blade.php
+++ resources/views/objects/parts/debts-details/workers_salary.blade.php
@@ -3,7 +3,7 @@
         @foreach($workSalaryDebtDetails as $detail)
             <div class="col-md-6 mb-4">
                 <strong class="{{ $detail['is_real'] ? '' : 'required' }}">{{ $detail['date'] }}</strong>
-                <span class="text-danger">{{ \App\Models\CurrencyExchangeRate::format($detail['amount'], 'RUB') }}</span>
+                <span class="{{ $detail['amount'] > 0 ? 'text-success' : 'text-danger' }}">{{ \App\Models\CurrencyExchangeRate::format($detail['amount'], 'RUB') }}</span>
             </div>
         @endforeach
     </div>
