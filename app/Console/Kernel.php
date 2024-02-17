5fb4d32f9cf334aa002817825455aae8e68acb1e
result <result007@yandex.ru>
2024-02-17 14:28:28 +0300

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('oms:objects-debts-from-excel')->dailyAt('19:00');
         $schedule->command('oms:service-debts-from-excel')->dailyAt('19:00');
         $schedule->command('oms:make-finance-report-history')->everyTenMinutes();
-        $schedule->command('oms:notify-to-email-about-object-balance')->dailyAt('15:00');
+//        $schedule->command('oms:notify-to-email-about-object-balance')->dailyAt('15:00');
     }
 
     /**
