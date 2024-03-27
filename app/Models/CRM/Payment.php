1cff7cf6cd1f20ad8b29d91b9da74643b82c3d89
result <result007@yandex.ru>
2024-03-28 00:43:56 +0300

    fix

--- app/Models/CRM/Payment.php
+++ app/Models/CRM/Payment.php
@@ -3,10 +3,16 @@
 namespace App\Models\CRM;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
     protected $table = 'payments';
 
     protected $connection = 'mysql_crm';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'u_id');
+    }
 }
