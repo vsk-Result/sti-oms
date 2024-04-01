db65d2c23520f3becf681aef937e465390c412c9
result <result007@yandex.ru>
2024-04-01 15:17:20 +0300

    fix

--- resources/views/sidebars/main.blade.php
+++ resources/views/sidebars/main.blade.php
@@ -66,7 +66,7 @@
                 @endcan
             @endcan
 
-            @if(auth()->user()->hasRole('super-admin'))
+            @if(auth()->user()->hasRole('super-admin') || auth()->id() === 16)
                 <div class="menu-item pt-5">
                     <div class="menu-content pb-2">
                         <span class="menu-section text-muted text-uppercase fs-7 fw-bolder"> Схемы</span>
