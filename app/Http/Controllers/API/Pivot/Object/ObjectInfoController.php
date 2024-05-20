77b85dcb7d6074c461f4aa73cc5b9baffc8296b9
result <result007@yandex.ru>
2024-05-20 23:28:59 +0300

    add salary debt in object pivot api, optimize

--- app/Http/Controllers/API/Pivot/Object/ObjectInfoController.php
+++ app/Http/Controllers/API/Pivot/Object/ObjectInfoController.php
@@ -3,24 +3,13 @@
 namespace App\Http\Controllers\API\Pivot\Object;
 
 use App\Http\Controllers\Controller;
-use App\Models\CurrencyExchangeRate;
-use App\Models\Debt\Debt;
-use App\Models\Debt\DebtImport;
+use App\Models\FinanceReportHistory;
 use App\Models\Object\BObject;
-use App\Services\PivotObjectDebtService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ObjectInfoController extends Controller
 {
-    private PivotObjectDebtService $pivotObjectDebtService;
-
-    public function __construct(
-        PivotObjectDebtService $pivotObjectDebtService
-    ) {
-        $this->pivotObjectDebtService = $pivotObjectDebtService;
-    }
-
     public function index(Request $request): JsonResponse
     {
         if (! $request->has('verify_hash')) {
@@ -32,6 +21,11 @@ class ObjectInfoController extends Controller
             abort(403);
             return response()->json([], 403);
         }
+        $financeReportHistory = FinanceReportHistory::where('date', now()->format('Y-m-d'))->first();
+
+        $objectsInfo = json_decode($financeReportHistory->objects_new);
+        $years = collect($objectsInfo->years)->toArray();
+        $total = $objectsInfo->total;
 
         $info = [
             'debts' => [],
@@ -40,69 +34,30 @@ class ObjectInfoController extends Controller
                 'contractors_debts_gu' => 0,
                 'providers_debts' => 0,
                 'service_debts' => 0,
+                'salary_debts' => 0,
                 'total_debts' => 0,
             ]
         ];
         $objects = BObject::active()->orderBy('code')->get();
 
         foreach ($objects as $object) {
-            $debts = $this->pivotObjectDebtService->getPivotDebtForObject($object->id);
+            $inf = [];
 
-            if ($object->code === '288') {
-                $contractorDebts = $debts['contractor']->debts;
-                $contractorDebtsAmount = 0;
-
-                foreach ($contractorDebts as $in) {
-                    $contractorDebtsAmount += array($in->worktype)[1] ?? 0;
-                    $contractorDebtsAmount += array($in->worktype)[2] ?? 0;
-                    $contractorDebtsAmount += array($in->worktype)[4] ?? 0;
-                    $contractorDebtsAmount += array($in->worktype)[7] ?? 0;
+            foreach ($years as $year => $objects) {
+                foreach ($objects as $o) {
+                    if ($o->id === $object->id) {
+                        $inf = (array) $total->{$year}->{$object->code};
+                        break;
+                    }
                 }
-
-                $providerDebts = $debts['provider']->debts;
-                $providerDebtsAmount = 0;
-
-                foreach ($providerDebts as $in) {
-                    $providerDebtsAmount += array($in->worktype)[1] ?? 0;
-                    $providerDebtsAmount += array($in->worktype)[2] ?? 0;
-                    $providerDebtsAmount += array($in->worktype)[4] ?? 0;
-                    $providerDebtsAmount += array($in->worktype)[7] ?? 0;
-                }
-
-                $totalDebts = $contractorDebtsAmount + $providerDebtsAmount;
-
-                $info['debts'][] = [
-                    'id' => $object->id,
-                    'object_name' => $object->getName(),
-                    'contractors_debts' => $contractorDebtsAmount,
-                    'contractors_debts_gu' => 0,
-                    'providers_debts' => $providerDebtsAmount,
-                    'total_debts' => $totalDebts,
-                ];
-
-                $info['total']['contractors_debts'] += $contractorDebtsAmount;
-                $info['total']['contractors_debts_gu'] = 0;
-                $info['total']['providers_debts'] += $providerDebtsAmount;
-                $info['total']['total_debts'] += $totalDebts;
-
-                continue;
             }
 
-            $contractorDebtsAmount = $debts['contractor']->total_amount;
-            $contractorDebtsAmountGU = 0;
-
-            $debtObjectImport = DebtImport::where('type_id', DebtImport::TYPE_OBJECT)->latest('date')->first();
-            $objectExistInObjectImport = $debtObjectImport->debts()->where('object_id', $object->id)->count() > 0;
-
-            if ($objectExistInObjectImport) {
-                $contractorDebtsAvans = Debt::where('import_id', $debtObjectImport->id)->where('type_id', Debt::TYPE_CONTRACTOR)->where('object_id', $object->id)->sum('avans');
-                $contractorDebtsAmountGU = Debt::where('import_id', $debtObjectImport->id)->where('type_id', Debt::TYPE_CONTRACTOR)->where('object_id', $object->id)->sum('guarantee');
-                $contractorDebtsAmount = $contractorDebtsAmount + $contractorDebtsAvans;
-            }
-
-            $providerDebtsAmount = $debts['provider']->total_amount;
-            $serviceDebtsAmount = $debts['service']->total_amount;
-            $totalDebts = $contractorDebtsAmount + $providerDebtsAmount + $serviceDebtsAmount + $contractorDebtsAmountGU;
+            $contractorDebtsAmount = $inf['contractor_debt'];
+            $contractorDebtsAmountGU = $inf['contractor_debt_gu'];
+            $providerDebtsAmount = $inf['provider_debt'];
+            $serviceDebtsAmount = $inf['service_debt'];
+            $salaryDebtsAmount = $inf['workers_salary_debt'];
+            $totalDebts = $contractorDebtsAmount + $providerDebtsAmount + $serviceDebtsAmount + $contractorDebtsAmountGU + $salaryDebtsAmount;
 
             $info['debts'][] = [
                 'id' => $object->id,
@@ -111,6 +66,7 @@ class ObjectInfoController extends Controller
                 'contractors_debts_gu' => $contractorDebtsAmountGU,
                 'providers_debts' => $providerDebtsAmount,
                 'service_debts' => $serviceDebtsAmount,
+                'salary_debts' => $salaryDebtsAmount,
                 'total_debts' => $totalDebts,
             ];
 
@@ -118,6 +74,7 @@ class ObjectInfoController extends Controller
             $info['total']['contractors_debts_gu'] += $contractorDebtsAmountGU;
             $info['total']['providers_debts'] += $providerDebtsAmount;
             $info['total']['service_debts'] += $serviceDebtsAmount;
+            $info['total']['salary_debts'] += $salaryDebtsAmount;
             $info['total']['total_debts'] += $totalDebts;
         }
 
