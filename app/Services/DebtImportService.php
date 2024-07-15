34aae19d558a3c70e952e090dff32f13a99ee03d
result <result007@yandex.ru>
2024-07-15 09:47:58 +0300

    fix

--- app/Services/DebtImportService.php
+++ app/Services/DebtImportService.php
@@ -88,6 +88,7 @@ class DebtImportService
         'Домодедово' => '308',
         'Кастанаевская' => '303',
         '367 Офис Веспер' => '367',
+        '371. ЗИЛ' => '371',
     ];
 
     public function __construct(
