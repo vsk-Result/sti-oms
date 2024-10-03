9a5aa5a1187a0b059de5d19181531595f0cab692
result <result007@yandex.ru>
2024-10-03 08:48:48 +0300

    fix

--- app/Models/Object/BObject.php
+++ app/Models/Object/BObject.php
@@ -287,7 +287,7 @@ class BObject extends Model implements Audit
         $exceptObjectsWithCodes = array_keys(self::getCodesWithoutWorktype());
         return $query->where(function($q) {
             $q->where('status_id', Status::STATUS_ACTIVE);
-//            $q->orWhere('code', '349');
+            $q->orWhere('code', '359');
         })->whereNotIn('code', $exceptObjectsWithCodes);
     }
 
