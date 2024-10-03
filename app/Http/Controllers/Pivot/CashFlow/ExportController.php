6d0285198fde5e457ce3a2918fec224ef17918c4
result <result007@yandex.ru>
2024-10-03 08:43:07 +0300

    fix

--- app/Http/Controllers/Pivot/CashFlow/ExportController.php
+++ app/Http/Controllers/Pivot/CashFlow/ExportController.php
@@ -23,37 +23,37 @@ class ExportController extends Controller
 
     public function store(): BinaryFileResponse
     {
-        if (auth()->id() === 1) {
-            $periods = $this->receivePlanService->getPeriods();
-            $planPaymentTypes = $this->receivePlanService->getPlanPaymentTypes();
-            foreach ($periods as $index => $period) {
-                foreach ($planPaymentTypes as $paymentType) {
-                    $planPayment = PlanPayment::where('name', $paymentType)->first();
-                    if (!$planPayment) {
-                        continue;
-                    }
-                    $entry = PlanPaymentEntry::where('payment_id', $planPayment->id)->where('date', $period['start'])->first();
-
-                    $amount = TaxPlanItem::where('paid', false)->where('name', $paymentType)->whereBetween('due_date', [$period['start'], $period['end']])->sum('amount');
-                    if ($index === 0) {
-                        $amount = TaxPlanItem::where('paid', false)->where('name', $paymentType)->where('due_date', '<=', $period['end'])->sum('amount');
-                    }
-
-                    if (!$entry) {
-                        PlanPaymentEntry::create([
-                            'payment_id' => $planPayment->id,
-                            'date' => $period['start'],
-                            'amount' => $amount,
-                            'status_id' => Status::STATUS_ACTIVE
-                        ]);
-                    } else {
-                        $entry->update([
-                            'amount' => $amount,
-                        ]);
-                    }
-                }
-            }
-        }
+//        if (auth()->id() === 1) {
+//            $periods = $this->receivePlanService->getPeriods();
+//            $planPaymentTypes = $this->receivePlanService->getPlanPaymentTypes();
+//            foreach ($periods as $index => $period) {
+//                foreach ($planPaymentTypes as $paymentType) {
+//                    $planPayment = PlanPayment::where('name', $paymentType)->first();
+//                    if (!$planPayment) {
+//                        continue;
+//                    }
+//                    $entry = PlanPaymentEntry::where('payment_id', $planPayment->id)->where('date', $period['start'])->first();
+//
+//                    $amount = TaxPlanItem::where('paid', false)->where('name', $paymentType)->whereBetween('due_date', [$period['start'], $period['end']])->sum('amount');
+//                    if ($index === 0) {
+//                        $amount = TaxPlanItem::where('paid', false)->where('name', $paymentType)->where('due_date', '<=', $period['end'])->sum('amount');
+//                    }
+//
+//                    if (!$entry) {
+//                        PlanPaymentEntry::create([
+//                            'payment_id' => $planPayment->id,
+//                            'date' => $period['start'],
+//                            'amount' => $amount,
+//                            'status_id' => Status::STATUS_ACTIVE
+//                        ]);
+//                    } else {
+//                        $entry->update([
+//                            'amount' => $amount,
+//                        ]);
+//                    }
+//                }
+//            }
+//        }
 
         return Excel::download(new Export($this->receivePlanService), 'Отчет CASH FLOW.xlsx');
     }
