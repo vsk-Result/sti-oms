d98f9a80d0614ac672b6a0a481496f6106f1ca1e
result <result007@yandex.ru>
2024-12-02 15:19:35 +0300

    fix

--- app/Models/CashCheck/CashCheck.php
+++ app/Models/CashCheck/CashCheck.php
@@ -113,10 +113,7 @@ class CashCheck extends Model implements Audit
     public function scopeForManager($query, int $managerId)
     {
         return $query->whereHas('managers', function ($q) use ($managerId) {
-            return $q->where(function($qq) use ($managerId) {
-                $qq->where('manager_id', $managerId);
-                $qq->orWhere('manager_id', 1);
-            });
+            return $q->where('manager_id', $managerId);
         });
     }
 
