0b77884c38df1549f94e9c47ad9b5d041f8fb495
result <result007@yandex.ru>
2024-12-19 09:17:15 +0300

    fix

--- app/Console/Commands/ImportObjectDebtsFromExcel.php
+++ app/Console/Commands/ImportObjectDebtsFromExcel.php
@@ -101,6 +101,7 @@ class ImportObjectDebtsFromExcel extends Command
         '367 Офис Веспер' => '367',
         '371. ЗИЛ' => '371',
         '372. Офис Веспер Тверская' => '372',
+        '376. Сбер К32' => '376',
     ];
 
     public function __construct(
