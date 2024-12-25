e06ceef51e1ce1c5027a204b0c675299e1b769d3
result <result007@yandex.ru>
2024-12-25 10:39:07 +0300

    fix

--- resources/views/sidebars/managers_objects.blade.php
+++ resources/views/sidebars/managers_objects.blade.php
@@ -1,14 +1,16 @@
 <button id="kt_explore_toggle_ao" class="explore-toggle btn btn-sm bg-body btn-color-gray-700 btn-active-primary shadow-sm position-fixed px-5 fw-bolder zindex-2 top-75 mt-17 end-0 transform-90 rounded-top-0" title="Финансовые менеджеры" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-trigger="hover">
     <span id="kt_explore_toggle_label">Финансовые менеджеры</span>
 </button>
-<div id="kt_explore" class="bg-body" data-kt-drawer="true" data-kt-drawer-name="explore" data-kt-drawer-activate="true" data-kt-drawer-overlay="false" data-kt-drawer-width="{default:'1000px', 'lg': '1000px'}" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_explore_toggle_ao" data-kt-drawer-close="#kt_explore_close">
+<div id="kt_explore" class="bg-body" data-kt-drawer="true" data-kt-drawer-name="explore" data-kt-drawer-activate="true" data-kt-drawer-overlay="false" data-kt-drawer-width="{default: {{ auth()->id() === 12 ? '200px' : '1000px' }}, 'lg': {{ auth()->id() === 12 ? '200px' : '1000px' }}}" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_explore_toggle_ao" data-kt-drawer-close="#kt_explore_close">
     <div class="card shadow-none rounded-0 w-100">
         <div class="card-header" id="kt_explore_header">
             <h3 class="card-title fw-bolder text-gray-700">
-                Распределение финансовых менеджеров по объектам
-                <a href="/storage/public/objects-debts-manuals/managers_objects.xlsx" class="btn btn-sm btn-light-success ms-4">
-                    Скачать в Excel
-                </a>
+                Финансовые менеджеры
+                <div>
+                    <a href="/storage/public/objects-debts-manuals/managers_objects.xlsx" class="btn btn-sm btn-light-success ms-4">
+                        Скачать в Excel
+                    </a>
+                </div>
             </h3>
             <div class="card-toolbar">
                 <button type="button" class="btn btn-sm btn-icon btn-active-light-primary me-n5" id="kt_explore_close">
@@ -30,9 +32,11 @@
                                 <tr class="fw-bold">
                                     <th class="ps-3" style="max-width: 105px;">Код объекта</th>
                                     <th style="max-width: 220px;">Объект</th>
-                                    <th style="max-width: 185px;">Фин.менеджер для согласования счетов / договоров</th>
-                                    <th style="max-width: 250px;">Почта</th>
-                                    <th style="max-width: 250px;">Телефон</th>
+                                    @if (auth()->id() !== 12)
+                                        <th style="max-width: 185px;">Фин.менеджер для согласования счетов / договоров</th>
+                                        <th style="max-width: 250px;">Почта</th>
+                                        <th style="max-width: 250px;">Телефон</th>
+                                    @endif
                                 </tr>
                             </thead>
                             <tbody>
@@ -42,24 +46,26 @@
                                     <tr>
                                         <td class="ps-3">{{ $manager['object_code'] }}</td>
                                         <td>{{ $manager['object_name'] }}</td>
-                                        <td>
-                                            @foreach($manager['names'] as $managerName)
-                                                {{ $managerName }}
-                                                <br>
-                                            @endforeach
-                                        </td>
-                                        <td>
-                                            @foreach($manager['emails'] as $email)
-                                                <a href="mailto:{{ $email }}">{{ $email }}</a>
-                                                <br>
-                                            @endforeach
-                                        </td>
-                                        <td>
-                                            @foreach($manager['phones'] as $phone)
-                                                <a href="tel:{{ str_replace(' ', '', $phone) }}">{{ $phone }}</a>
-                                                <br>
-                                            @endforeach
-                                        </td>
+                                        @if (auth()->id() !== 12)
+                                            <td>
+                                                @foreach($manager['names'] as $managerName)
+                                                    {{ $managerName }}
+                                                    <br>
+                                                @endforeach
+                                            </td>
+                                            <td>
+                                                @foreach($manager['emails'] as $email)
+                                                    <a href="mailto:{{ $email }}">{{ $email }}</a>
+                                                    <br>
+                                                @endforeach
+                                            </td>
+                                            <td>
+                                                @foreach($manager['phones'] as $phone)
+                                                    <a href="tel:{{ str_replace(' ', '', $phone) }}">{{ $phone }}</a>
+                                                    <br>
+                                                @endforeach
+                                            </td>
+                                        @endif
                                     </tr>
                                 @endforeach
                             </tbody>
