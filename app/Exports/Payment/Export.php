c905fc125c89d0c32c08e33633ede4d109f92259
result <result007@yandex.ru>
2025-01-15 10:55:58 +0300

    fix

--- app/Exports/Payment/Export.php
+++ app/Exports/Payment/Export.php
@@ -32,12 +32,12 @@ class Export implements WithMultipleSheets
             $object = BObject::find($objectIds[0]);
             $objectName = $object ? $object->getName() : '';
         }
-
-        if (auth()->id() === 1) {
-            return [
-                new BackupSheet('История'),
-            ];
-        }
+//
+//        if (auth()->id() === 1) {
+//            return [
+//                new BackupSheet('История'),
+//            ];
+//        }
 
         return [
             new PivotSheet('Сводная', (clone $this->payments), $objectName),
