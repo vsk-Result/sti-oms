e8267d016fd74c0599561eba6f72b16bd23281e5
result <result007@yandex.ru>
2025-02-14 11:35:48 +0300

    add fix codes for general costs

--- app/Models/Object/GeneralCost.php
+++ app/Models/Object/GeneralCost.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Object;
 
+use App\Models\Payment;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -18,12 +19,24 @@ class GeneralCost extends Model
 
     public static function getObjectCodesForGeneralCosts(): array
     {
-        // исключили 343 по письму от Оксаны 30 мая 2023
-        return [
+        $fixedCodes = [
             '257', '268', '288', '292', '296', '298', '303', '304', '305', '308', '309', '317', '321', '322',
             '323', '325', '327', '330', '332', '333', '334', '335', '338', '339', '341', '342',
             '344', '346', '349', '350', '352', '353', '358', '359', '360', '361', '363', '364', '365', '366', '367', '369',
             '371', '373', '374'
         ];
+
+        $newCodes = [];
+
+        foreach (BObject::where('code', '>', '374')->orderBy('code')->get() as $object) {
+            if ($object->payments
+                    ->whereIn('organization_sender_id', $object->customers->pluck('id')->toArray())
+                    ->sum('amount')
+                > 0) {
+                $newCodes[] = $object->code;
+            }
+        }
+
+        return array_merge($fixedCodes, $newCodes);
     }
 }
