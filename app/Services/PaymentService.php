47b848fafa53e3e20038bc1b0939f068e53a2c9f
result <result007@yandex.ru>
2025-08-12 12:22:37 +0300

    fix zp slit

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -506,7 +506,7 @@ class PaymentService
             || str_contains($description, 'перечисление согласно реестру')
             || str_contains($description, 'аванс сотрудников ооо "строй техно инженеринг"')
             || str_contains($description, 'заработная плата сотрудников ооо "строй техно инженеринг"')
-            || str_contains($description, 'Z_0000006223')
+            || str_contains($description, 'по реестру Z_0000036')
             || str_contains($description, 'заработная плата сотрудниковооо')
         ) {
             return true;
