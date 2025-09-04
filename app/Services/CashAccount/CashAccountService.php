716c2bcb1bbfc2c075223eca60083949f2656fc9
result <result007@yandex.ru>
2025-09-04 14:14:16 +0300

    fix

--- app/Services/CashAccount/CashAccountService.php
+++ app/Services/CashAccount/CashAccountService.php
@@ -71,7 +71,7 @@ class CashAccountService
     public function updateBalance(CashAccount $cashAccount): void
     {
         $cashAccount->update([
-            'balance_amount' => $cashAccount->start_balance_amount + $cashAccount->payments()->where('status_id', Status::STATUS_ACTIVE)->sum('amount')
+            'balance_amount' => $cashAccount->start_balance_amount + $cashAccount->payments()->whereIn('status_id', [CashAccountPayment::STATUS_ACTIVE, CashAccountPayment::STATUS_VALID])->sum('amount')
         ]);
     }
 
