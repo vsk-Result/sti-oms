1047b15176d0d41850333ab389ad01a6723b7b8f
result <result007@yandex.ru>
2025-09-17 12:38:51 +0300

    add fix doublse

---
+++ app/Console/Commands/RemoveDubles.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Console\HandledCommand;
+use App\Models\CashAccount\CashAccountPayment;
+use App\Models\CRM\CostItem;
+use App\Models\KostCode;
+use App\Models\Payment;
+
+class RemoveDubles extends HandledCommand
+{
+    protected $signature = 'oms:rename-dubles';
+
+    protected $description = 'la la la la la la a';
+
+    protected string $period = 'Вручную';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        if ($this->isProcessRunning()) {
+            return 0;
+        }
+
+        $this->startProcess();
+
+        $payments = Payment::whereBetween('date', ['2025-07-01', '2025-09-30'])
+                    ->where('payment_type_id', Payment::PAYMENT_TYPE_CASH)->get();
+
+        $logs = [];
+
+        foreach ($payments as $payment) {
+            $doublePayments = Payment::whereBetween('date', ['2025-07-01', '2025-09-30'])
+                ->where('payment_type_id', Payment::PAYMENT_TYPE_CASH)
+                ->where('id', '!=', $payment->id)
+                ->where('description', $payment->description)
+                ->where('amount', $payment->amount)
+                ->where('code', $payment->code)
+                ->get();
+
+           if ($doublePayments->count() > 0) {
+               $logs[] = 'Для оплаты ' . $payment->id . ' найдено ' . $doublePayments->count() . ' дублей';
+           }
+        }
+
+        $this->endProcess();
+
+        dd($logs);
+
+        return 0;
+    }
+}
