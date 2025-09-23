31e5c5e832e419e15e9a4bb721c021c9aab6e619
result <result007@yandex.ru>
2025-09-23 12:24:50 +0300

    fix warning

--- resources/views/partials/check_organization.blade.php
+++ resources/views/partials/check_organization.blade.php
@@ -2,17 +2,31 @@
     $warningInfo = null;
 
     if (isset($organizationInn) || isset($organizationName)) {
+
+        $amount = 0;
         $warningOrganizationsInfo = \Illuminate\Support\Facades\Cache::get('warning_organizations_data', []);
 
         if (isset($organizationInn)) {
             $foundByInn = array_search($organizationInn, array_column($warningOrganizationsInfo, 'inn'));
             if ($foundByInn !== false) {
                 $warningInfo = $warningOrganizationsInfo[$foundByInn];
+
+                foreach ($warningOrganizationsInfo as $info) {
+                    if ($info['inn'] == $organizationInn) {
+                        $amount += $info['amount'];
+                    }
+                }
             }
         } elseif (isset($organizationName)) {
             $foundByName = array_search($organizationName, array_column($warningOrganizationsInfo, 'organizationName'));
             if ($foundByName !== false) {
                 $warningInfo = $warningOrganizationsInfo[$foundByName];
+
+                foreach ($warningOrganizationsInfo as $info) {
+                    if ($info['organizationName'] == $organizationName) {
+                        $amount += $info['amount'];
+                    }
+                }
             }
         }
     }
@@ -25,7 +39,7 @@
             data-bs-placement="top"
             data-bs-html="true"
             data-bs-delay-hide="1000"
-            title="{{ $warningInfo['type'] . ' на сумму ' . \App\Models\CurrencyExchangeRate::format($warningInfo['amount']) }}, <a href='{{ route('files.download', ['file' => base64_encode('public/objects-debts-manuals/warning_organizations.xls')]) }}'>Скачать детали</a>"
+            title="{{ $warningInfo['type'] . ' на сумму ' . \App\Models\CurrencyExchangeRate::format($amount) }}, <a href='{{ route('files.download', ['file' => base64_encode('public/objects-debts-manuals/warning_organizations.xls')]) }}'>Скачать детали</a>"
     >{{ $organizationName ?? '' }}</span>
 @else
     <span>{{ $organizationName ?? '' }}</span>
