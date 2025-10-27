2babb1cfa59df6d7d1c3d28e8a66b7ebc9e9aa3c
result <result007@yandex.ru>
2025-10-27 10:29:12 +0300

    fix

--- app/Services/Pivots/CalculateWorkersCost/CalculateWorkersCostService.php
+++ app/Services/Pivots/CalculateWorkersCost/CalculateWorkersCostService.php
@@ -299,8 +299,8 @@ class CalculateWorkersCostService
 
                         $info['data'][$group]['total']['rate']['total'] = $info['total']['hours']['total'] === 0 ? 0 : $info['data'][$group]['total']['amount']['total'] / $info['total']['hours']['total'];
 
-                        $info['data'][$group]['rate'][$year['name']][$quart['name']]['total'] = $info['hours'][$year['name']][$quart['name']]['total'] === 0 ? 0 : $info['amount'][$group]['rate'][$year['name']][$quart['name']]['total'] / $info['hours'][$year['name']][$quart['name']]['total'];
-                        $info['data'][$group]['rate'][$year['name']]['total'] = $info['hours'][$year['name']]['total'] === 0 ? 0 : $info['amount'][$group]['rate'][$year['name']]['total'] / $info['hours'][$year['name']]['total'];
+                        $info['data'][$group]['rate'][$year['name']][$quart['name']]['total'] = $info['hours'][$year['name']][$quart['name']]['total'] === 0 ? 0 : $info['data'][$group]['amount'][$year['name']][$quart['name']]['total'] / $info['hours'][$year['name']][$quart['name']]['total'];
+                        $info['data'][$group]['rate'][$year['name']]['total'] = $info['hours'][$year['name']]['total'] === 0 ? 0 : $info['data'][$group]['amount'][$year['name']]['total'] / $info['hours'][$year['name']]['total'];
                     }
 
                     $info['total']['rate']['total'] = $info['total']['hours']['total'] === 0 ? 0 : $info['total']['amount']['total'] / $info['total']['hours']['total'];
