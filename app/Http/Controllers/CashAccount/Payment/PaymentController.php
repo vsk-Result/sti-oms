e275fda457683e9dd3c08c3479a5753725b43df8
result <result007@yandex.ru>
2025-11-11 22:54:46 +0300

    fix

--- app/Http/Controllers/CashAccount/Payment/PaymentController.php
+++ app/Http/Controllers/CashAccount/Payment/PaymentController.php
@@ -56,6 +56,8 @@ class PaymentController extends Controller
             $activeOrganizations = Organization::whereIn('id', $request->get('organization_id'))->orderBy('name')->get();
         }
 
+        dd($payments);
+
         return view(
             'cash-accounts.payments.index',
             compact(
