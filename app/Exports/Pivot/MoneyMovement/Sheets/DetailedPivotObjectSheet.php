6bfc0a5e53f7b708306c1019dc8c3f29aeb8a9a7
result <result007@yandex.ru>
2025-11-19 02:34:57 +0300

    fix

--- app/Exports/Pivot/MoneyMovement/Sheets/DetailedPivotObjectSheet.php
+++ app/Exports/Pivot/MoneyMovement/Sheets/DetailedPivotObjectSheet.php
@@ -36,6 +36,7 @@ class DetailedPivotObjectSheet implements
 
         $minDate = (clone $this->payments)->min('date');
         $maxDate = (clone $this->payments)->max('date');
+        $period = [$minDate, $maxDate];
 
         $sheet->setCellValue('A2', 'Период с ' . Carbon::parse($minDate)->format('d.m.Y') . ' по ' . Carbon::parse($maxDate)->format('d.m.Y'));
 
@@ -43,73 +44,84 @@ class DetailedPivotObjectSheet implements
         $sheet->setCellValue('B3', Payment::where('date', '<=', $minDate)->sum('amount'));
 
         $sheet->setCellValue('A4', 'Итого приход');
-        $sheet->setCellValue('B4', Payment::whereBetween('date', [$minDate, $maxDate])->where('amount', '>=', 0)->sum('amount'));
+        $sheet->setCellValue('B4', Payment::whereBetween('date', $period)->where('amount', '>=', 0)->sum('amount'));
 
         $sheet->setCellValue('A5', 'Итого расход');
-        $sheet->setCellValue('B5', Payment::whereBetween('date', [$minDate, $maxDate])->where('amount', '<', 0)->sum('amount'));
+        $sheet->setCellValue('B5', Payment::whereBetween('date', $period)->where('amount', '<', 0)->sum('amount'));
 
         $sheet->setCellValue('A6', 'Остаток на конец ' . Carbon::parse($maxDate)->format('d.m'));
         $sheet->setCellValue('B6', Payment::where('date', '<=', $maxDate)->sum('amount'));
 
-        $sheet->getStyle('A1:D1')->getAlignment()->setVertical('center')->setHorizontal('center')->setWrapText(false);
+        $sheet->getStyle('A2:B2')->getAlignment()->setVertical('center')->setHorizontal('center')->setWrapText(false);
         $sheet->getRowDimension(2)->setRowHeight(30);
 
         $sheet->getColumnDimension('A')->setWidth(26);
         $sheet->getColumnDimension('B')->setWidth(17);
-        $sheet->getColumnDimension('C')->setWidth(12);
-        $sheet->getColumnDimension('D')->setWidth(12);
 
-        $sheet->getStyle('A1:D7')->getFont()->setBold(true);
-        $sheet->mergeCells('A2:D2');
+        $sheet->getStyle('A1:B7')->getFont()->setBold(true);
+        $sheet->mergeCells('A2:B2');
+
+        $sheet->getStyle('A2:B2')->applyFromArray([
+            'borders' => ['outline' => ['borderStyle' => Border::BORDER_MEDIUM]]
+        ]);
 
         $objectIds = (clone $this->payments)->where('type_id', Payment::TYPE_OBJECT)->groupBy('object_id')->pluck('object_id')->toArray();
         $objects = BObject::whereIn('id', $objectIds)->orderByDesc('code')->get();
 
         $row = 9;
-        $totals = [
-            'receive' => 0,
-            'payment' => 0,
-        ];
 
         foreach ($objects as $object) {
             if ($object->code === '27.1') {
                 continue;
             }
 
-            $sheet->getStyle('A' . $row . ':B' . ($row + 3))->applyFromArray([
-                'borders' => ['outline' => ['borderStyle' => Border::BORDER_THICK]]
+            $this->fillObjectInfo($sheet, $row, [
+                'title' => $object->getName(),
+                'receive' => $object->payments()->whereBetween('date', [$minDate, $maxDate])->where('amount', '>=', 0)->sum('amount'),
+                'payment' => $object->payments()->whereBetween('date', [$minDate, $maxDate])->where('amount', '<', 0)->sum('amount'),
+                'period' => $period,
             ]);
 
-            $receive = $object->payments()->whereBetween('date', [$minDate, $maxDate])->where('amount', '>=', 0)->sum('amount');
-            $payment = $object->payments()->whereBetween('date', [$minDate, $maxDate])->where('amount', '<', 0)->sum('amount');
-
-            $totals['receive'] += $receive;
-            $totals['payment'] += $payment;
-
-            $sheet->setCellValue('A' . $row, $object->getName());
-
-            $sheet->getStyle('A' . $row)->getFont()->setBold(true);
-
-            $row++;
-            $sheet->setCellValue('A' . $row, 'Приход');
-            $sheet->setCellValue('B' . $row, $receive);
-            $row++;
-            $sheet->setCellValue('A' . $row, 'Расход');
-            $sheet->setCellValue('B' . $row, $payment);
-            $row++;
-            $sheet->setCellValue('A' . $row, 'Сальдо');
-            $sheet->setCellValue('B' . $row, $receive + $payment);
-            $row++;
-
-//            $sheet->getRowDimension($row)->setRowHeight(30);
+            $row += 3;
         }
 
-        $sheet->setCellValue('A' . $row, 'Офис');
+        $row++;
+
+        $office = BObject::where('code', '27.1')->first();
+
+        $this->fillObjectInfo($sheet, $row, [
+            'title' => 'Офис',
+            'receive' => $office->payments()->whereBetween('date', [$minDate, $maxDate])->where('amount', '>=', 0)->sum('amount'),
+            'payment' => $office->payments()->whereBetween('date', [$minDate, $maxDate])->where('amount', '<', 0)->sum('amount'),
+            'period' => $period,
+        ]);
+
         $row++;
 
         $row++;
         $sheet->setCellValue('A' . $row, 'Общие затраты');
 
         $sheet->getStyle('B3:B' . $row)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
+    }
+
+    public function fillObjectInfo(&$sheet, $row, array $info)
+    {
+        $sheet->getStyle('A' . $row . ':B' . ($row + 3))->applyFromArray([
+            'borders' => ['outline' => ['borderStyle' => Border::BORDER_MEDIUM]]
+        ]);
+
+        $sheet->setCellValue('A' . $row, $info['title']);
+
+        $sheet->getStyle('A' . $row)->getFont()->setBold(true);
+
+        $row++;
+        $sheet->setCellValue('A' . $row, 'Приход');
+        $sheet->setCellValue('B' . $row, $info['receive']);
+        $row++;
+        $sheet->setCellValue('A' . $row, 'Расход');
+        $sheet->setCellValue('B' . $row, $info['payment']);
+        $row++;
+        $sheet->setCellValue('A' . $row, 'Сальдо');
+        $sheet->setCellValue('B' . $row, $info['receive'] + $info['payment']);
     }
 }
