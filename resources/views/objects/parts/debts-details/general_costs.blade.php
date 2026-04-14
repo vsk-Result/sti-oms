c6db3b02f8bb80b74a18e2151a0187cfcdaa8803
result <result007@yandex.ru>
2026-04-14 14:15:29 +0300

    add details general costs in object

--- resources/views/objects/parts/debts-details/general_costs.blade.php
+++ resources/views/objects/parts/debts-details/general_costs.blade.php
@@ -4,18 +4,29 @@
 
         <div class="d-flex flex-column align-items-end">
             <span class="text-danger">{{ \App\Models\CurrencyExchangeRate::format($info['transfer_service'], 'RUB') }}</span>
-            <span class="text-muted fw-bold d-block fs-8">{{ \App\Models\CurrencyExchangeRate::format($info['transfer_service_without_nds'], 'RUB') }} без НДС</span>
+{{--            <span class="text-muted fw-bold d-block fs-8">{{ \App\Models\CurrencyExchangeRate::format($info['transfer_service_without_nds'], 'RUB') }} без НДС</span>--}}
         </div>
     </div>
 
     <div class="separator separator-dashed my-3"></div>
 
     <div class="d-flex flex-row justify-content-between">
-        <strong>Расходы офиса, налоги</strong>
+        <strong>Расходы центрального офиса</strong>
 
         <div class="d-flex flex-column align-items-end">
             <span class="text-danger">{{ \App\Models\CurrencyExchangeRate::format($info['office_service'], 'RUB') }}</span>
-            <span class="text-muted fw-bold d-block fs-8">{{ \App\Models\CurrencyExchangeRate::format($info['office_service_without_nds'], 'RUB') }} без НДС</span>
+{{--            <span class="text-muted fw-bold d-block fs-8">{{ \App\Models\CurrencyExchangeRate::format($info['office_service_without_nds'], 'RUB') }} без НДС</span>--}}
+        </div>
+    </div>
+
+    <div class="separator separator-dashed my-3"></div>
+
+    <div class="d-flex flex-row justify-content-between">
+        <strong>НДС, налог на прибыль</strong>
+
+        <div class="d-flex flex-column align-items-end">
+            <span class="text-danger">{{ \App\Models\CurrencyExchangeRate::format($info['general_nds'] ?? 0, 'RUB') }}</span>
+{{--            <span class="text-muted fw-bold d-block fs-8">{{ \App\Models\CurrencyExchangeRate::format($info['office_service_without_nds'], 'RUB') }} без НДС</span>--}}
         </div>
     </div>
 </div>
