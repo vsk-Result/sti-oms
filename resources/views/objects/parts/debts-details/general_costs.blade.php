79a993565826666c791f3296798d4a4473a2e01b
result <result007@yandex.ru>
2026-04-28 12:20:50 +0300

    fix

--- resources/views/objects/parts/debts-details/general_costs.blade.php
+++ resources/views/objects/parts/debts-details/general_costs.blade.php
@@ -29,4 +29,12 @@
 {{--            <span class="text-muted fw-bold d-block fs-8">{{ \App\Models\CurrencyExchangeRate::format($info['office_service_without_nds'], 'RUB') }} без НДС</span>--}}
         </div>
     </div>
+
+    <div class="d-flex flex-row justify-content-between">
+        <strong>% от прихода заказчика</strong>
+
+        <div class="d-flex flex-column align-items-end">
+            <span class="text-danger">{{ \App\Models\CurrencyExchangeRate::format($info['receive_customer'] != 0 ? (($info['general_nds'] ?? 0) / $info['receive_customer']) : 0, 'RUB') }}</span>
+        </div>
+    </div>
 </div>
