e0c414aa3dd1ee5d192dfae648b55628aa12d2cd
result <result007@yandex.ru>
2026-06-05 09:55:24 +0300

    fix

--- app/Models/CashAccount/CashAccountPayment.php
+++ app/Models/CashAccount/CashAccountPayment.php
@@ -149,11 +149,11 @@ class CashAccountPayment extends Model implements Audit, HasMedia
         $crmApartmentData = $this->getCrmApartmentData();
         $itrData = $this->getItrData();
 
-        if (! is_null($crmAvansData['employee_name']) && $this->code === '7.8.2' && !$crmAvansData['crm_not_need_avans']) {
+        if (! is_null($crmAvansData['employee_name']) && $this->code === '7.8.2') {
             $description .= ', выплата аванса ' . $crmAvansData['employee_name'] . ' за ' . $crmAvansData['date'];
         }
 
-        if (! is_null($crmAvansData['employee_name']) && $this->code === '7.9.2' && !$crmAvansData['crm_not_need_avans']) {
+        if (! is_null($crmAvansData['employee_name']) && $this->code === '7.9.2') {
             $description .= ', выплата зарплаты ' . $crmAvansData['employee_name'] . ' за ' . $crmAvansData['date'];
         }
 
