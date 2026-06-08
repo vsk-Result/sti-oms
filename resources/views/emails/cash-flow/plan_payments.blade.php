d77bc575d5e0d1a6413d2b7089e5d62f720c7b9a
result <result007@yandex.ru>
2026-06-08 22:25:44 +0300

    fix

--- resources/views/emails/cash-flow/plan_payments.blade.php
+++ resources/views/emails/cash-flow/plan_payments.blade.php
@@ -5,7 +5,7 @@
 
     <ul>
         @foreach($payments as $paymentName => $amount)
-            <li>{{ $paymentName }} - на сумму {{ \App\Models\CurrencyExchangeRate::format($amount) }}</li>
+            <li><strong>{{ $paymentName }}</strong> - на сумму <strong class="text-danger">{{ \App\Models\CurrencyExchangeRate::format($amount) }}</li>
         @endforeach
     </ul>
 @else
