eb9bff36d5305bd598fc352d2256f710507d9cc8
result <result007@yandex.ru>
2026-08-11 13:06:24 +0300

    fix

--- app/Console/Commands/ImportServiceDebtsFrom1CExcel.php
+++ app/Console/Commands/ImportServiceDebtsFrom1CExcel.php
@@ -36,6 +36,7 @@ class ImportServiceDebtsFrom1CExcel extends HandledCommand
         try {
             $data = $this->debtImportService->importDebts($import);
         } catch (\Exception $e) {
+            dd($e);
             $this->sendErrorMessage('Не удалось загрузить файл для импорта долгов по услугам "' . $import->getFilename() . '"');
             $this->sendErrorMessage($e->getMessage());
             $this->endProcess();
@@ -63,6 +64,7 @@ class ImportServiceDebtsFrom1CExcel extends HandledCommand
                 $import->getSource(),
             );
         } catch (\Exception $e) {
+            dd($e);
             $this->sendErrorMessage('Не удалось обновить сводные данные долгов по услугам');
             $this->sendErrorMessage($e->getMessage());
             $this->endProcess();
