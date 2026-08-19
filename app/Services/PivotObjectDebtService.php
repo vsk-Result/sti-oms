4a1f988d4a3e1127505d5ff82aa39b9594854293
result <result007@yandex.ru>
2026-08-19 13:27:39 +0300

    fix

--- app/Services/PivotObjectDebtService.php
+++ app/Services/PivotObjectDebtService.php
@@ -288,17 +288,21 @@ class PivotObjectDebtService
             ->where('debt_source_id', $debtSource)
             ->first();
 
-        if ($debtType === 2) {
-            dd($pivot, $debtType, $debtSource);
-        }
-
         if (! $pivot) {
+            if ($debtType === 2) {
+                dd($data, $pivot, $debtType, $debtSource);
+            }
+
             $pivot = PivotObjectDebt::create([
                 'date' => now(),
                 'object_id' => $data['object_id'],
                 'debt_type_id' => $debtType,
                 'debt_source_id' => $debtSource,
             ]);
+        }
+
+        if ($debtType === 2) {
+            dd($data, $pivot, $debtType, $debtSource);
         }
 
         $pivot->update([
